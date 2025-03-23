7e5d5bbe97311c863f95f1667a093133480c24ef
Demo <mehaljain2005@gmail.com>
2025-03-23 22:56:49 +0530

    Comment change

--- Project.py
+++ Project.py
@@ -18,7 +18,7 @@ def marble_betting_game():
             print("Invalid input. Please enter a number.")
             continue
         
-        marbles = ['white'] * 4 + ['black'] * 6  # 5 white, 5 black
+        marbles = ['white'] * 4 + ['black'] * 6  # 4 white, 6 black
         drawn_marble = random.choice(marbles)
         print(f"You drew a {drawn_marble} marble!")
         
